cfdf634fafa6aacec0df2dd49c9d3b8d5ad55a36
Debkumarkatwa <159232491+Debkumarkatwa@users.noreply.github.com>
2024-03-18 13:52:31 +0530

    Update 1_insertion_deletion_with_SWITCH.c

--- 1_insertion_deletion_with_SWITCH.c
+++ 1_insertion_deletion_with_SWITCH.c
@@ -1,5 +1,108 @@
 #include <stdio.h>
 
+void print(int arr[], int s);
+
+void insertion(int arr[], int *s, int indx);
+
+void deletion(int arr[], int *s, int indx);
+
+void searching(int arr[], int s, int key);
+
+void sorting(int arr[], int s);
+
+int main()
+{
+
+    int array[10], size;
+    printf("Enter the size of the array:: ");
+    scanf(" %d", &size);
+
+    if (size > 10)
+    {
+        printf("SORRY!! But the maximum limit is 10 only.\n");
+    }
+    else
+    {
+        printf("Enter the elements of the array:\n");
+
+        for (int i = 0; i < size; i++)
+        {
+            printf("Element %d: ", i + 1);
+            scanf("%d", &array[i]);
+        }
+        // Printing the original array
+        print(array, size);
+
+        int option;
+        printf("\n=====================================================");
+        printf("\n\t\t\t\t\t\tMenu\n");
+        printf("======================================================");
+        printf("\nA: Insert an Element at a specified position [Enter 1].");
+        printf("\nB: Delete an Element from a specified position [Enter 2].");
+        printf("\nC: Search an Element in the Array the Array[Enter 3].");
+        printf("\nF: Sort the Elements of the Array [Enter 4].");
+        printf("\nE: Display the Array [Enter 5].");
+        printf("\nF: Exit [Enter 6].");
+
+        do
+        {
+            printf("\n\n------------------------------------------");
+            printf("\n   Enter What You Want to Do[for exit (6)]: ");
+            scanf("%d", &option);
+
+            switch (option)
+            {
+            case 1:
+                int index;
+                printf("\nEnter the POSITION where you want to Insert the Element:: ");
+                scanf("%d", &index);
+
+                insertion(array, &size, index);
+                
+                printf("Insertion is Completed......");
+                break;
+
+            case 2:
+                int delIndex;
+                printf("\nEnter the POSITION of which Element you want to Delete: ");
+                scanf("%d", &delIndex);
+
+                deletion(array, &size, delIndex);
+                
+                printf("Deletion is Completed......");
+                break;
+
+            case 3:
+                int value;
+                printf("\nEnter the Element that you want to Search: ");
+                scanf("%d", &value);
+
+                searching(array, size, value);
+                break;
+
+            case 4:
+                sorting(array,size);
+                printf("Sorting is Completed......");
+                break;
+
+            case 5:
+                print(array, size);
+                break;
+            
+            case 6:
+                printf("Thank you for using.......");
+                break;
+            }
+            if (option > 6 || option < 1)
+            {
+                printf("SORRY!! You choose a Invalid option.....");
+                printf("Please Choose a Correct Option.....");
+            }
+        } while (option != 6);
+    }
+    return 0;
+}
+
 void print(int arr[], int s)
 {
     printf("\nThe Array is :::-----------------------");
@@ -11,7 +114,6 @@ void print(int arr[], int s)
 
 void insertion(int arr[], int *s, int indx)
 {
-    printf("%d\n", *s);
     int element;
     if (indx < 1 || indx > *s)
     {
@@ -64,84 +166,22 @@ void searching(int arr[], int s, int key)
     }
 }
 
-int main()
+void sorting(int arr[], int s)
 {
+    int flag=0;
+	for(int i=0; i<s-1; i++){
+        for(int j=0; j<s-1-i; j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j+1];    
+                arr[j+1]=arr[j];         
+                arr[j]=temp;
 
-    int array[10], size;
-    printf("Enter the size of the array:: ");
-    scanf(" %d", &size);
-
-    if (size > 10)
-    {
-        printf("SORRY!! But the maximum limit is 10 only.\n");
-    }
-    else
-    {
-        printf("Enter the elements of the array:\n");
-
-        for (int i = 0; i < size; i++)
-        {
-            printf("Element %d: ", i + 1);
-            scanf("%d", &array[i]);
+                flag++;
+            }
         }
-        // Printing the original array
-        print(array, size);
-
-        int option;
-        printf("\n=====================================");
-        printf("\n\t\tMenu\n");
-        printf("======================================");
-        printf("\nA: Insert an Element at a specified position [Enter 1].");
-        printf("\nB: Delete an Element from a specified position [Enter 2].");
-        printf("\nC: Search an Element in the Array the Array[Enter 3].");
-        printf("\nD: Display the Array [Enter 4].");
-        printf("\nE: Exit [Enter 5].");
-
-        do
-        {
-            printf("\n\n------------------------------------------");
-            printf("\n   Enter What You Want to Do: ");
-            scanf("%d", &option);
-
-            switch (option)
-            {
-            case 1:
-                int index;
-                printf("\nEnter the POSITION where you want to Insert the Element:: ");
-                scanf("%d", &index);
-
-                insertion(array, &size, index);
-                break;
-
-            case 2:
-                int delIndex;
-                printf("\nEnter the POSITION of which Element you want to Delete: ");
-                scanf("%d", &delIndex);
-
-                deletion(array, &size, delIndex);
-                break;
-
-            case 3:
-                int value;
-                printf("\nEnter the Element that you want to Search: ");
-                scanf("%d", &value);
-
-                searching(array, size, value);
-                break;
-
-            case 4:
-                print(array, size);
-                break;
-
-            case 5:
-                printf("Thank you for using.......");
-                break;
-            }
-            if (option > 5 || option < 1)
-            {
-                printf("SORRY!! You choose a Invalid option.....");
-                printf("Please Choose a Correct Option.....");
-            }
-        } while (option != 5);
+        if(flag==0){
+            printf("\n The array is already Sorted");
+            break;
+        }
     }
 }
